98a2331c9825aef9ebf91214f740d5f8455401b0
Sergey Shambir <sshambir@yandex.ru>
2018-06-01 13:14:10 +0300

    Added new console application

---
+++ labs/TextListener/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace TextListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
