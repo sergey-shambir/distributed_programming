ae7ff7e0a808f759eb5f2b1b5ac0e7c0c86c5488
Sergey Shambir <sshambir@yandex.ru>
2018-06-18 22:20:31 +0300

    Updated scripts to build/run/stop, added config file support for script

--- labs/share/run.sh
+++ labs/share/run.sh
@@ -3,8 +3,37 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOG_DIR="${SCRIPT_DIR}/logs"
 
+# Read config file
+IFS="="
+while read -r name value
+do
+if ! [[ -z "$name" ]]
+then
+    export $name=$value
+fi
+done < "${SCRIPT_DIR}/services.conf"
+
+# Usage: runDotNetApp <ProjectName>
+runDotNetApp() {
+    nohup dotnet "${SCRIPT_DIR}/$1/$1.dll" 1>"${LOG_DIR}/$1.out.log" 2>"${LOG_DIR}/$1.err.log" &
+}
+
+# Usage: runDotNetApp <Count> <ProjectName>
+# <Count> should be in range 1..32
+runDotNetAppMulti() {
+    if [[ $1 -lt 1 ]] || [[ $1 -gt 32 ]] ; then
+        echo "wrong count for runDotNetAppMulti"
+        exit
+    fi
+    for ((i=0; i<$1; i++)); do
+        runDotNetApp "$2"
+    done
+}
+
 mkdir -p "${LOG_DIR}"
-nohup dotnet "${SCRIPT_DIR}/Frontend/Frontend.dll" 1>"${LOG_DIR}/frontend.log" 2>"${LOG_DIR}/frontend_errors.log" &
-nohup dotnet "${SCRIPT_DIR}/Backend/Backend.dll" 1>"${LOG_DIR}/backend.log" 2>"${LOG_DIR}/backend_errors.log" &
-nohup dotnet "${SCRIPT_DIR}/TextListener/TextListener.dll" 1>"${LOG_DIR}/text_listener.log" 2>"${LOG_DIR}/text_listener_errors.log" &
-nohup dotnet "${SCRIPT_DIR}/TextRancCalc/TextRancCalc.dll" 1>"${LOG_DIR}/text_ranc_calc.log" 2>"${LOG_DIR}/text_ranc_calc_errors.log" &
+runDotNetApp "Frontend"
+runDotNetApp "Backend"
+runDotNetApp "TextListener"
+runDotNetApp "TextRancCalc"
+runDotNetAppMulti $NumVowelConsRater "VowelConsRater"
+runDotNetAppMulti $NumVowelConsCounter "VowelConsCounter"
